c94581e3c466c89c74009d21da6c0d86b9305119
fabisaurier <fabian.kinze@googlemail.com>
2025-03-08 21:02:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const NEWS_SOURCES = {
         logo: 'images/tagesschau_logo.png',
     },
     zeit: {
-        rss: 'https://newsfeed.zeit.de/',
+        rss: 'https://newsfeed.zeit.de',
         logo: 'images/zeit_logo.png',
     },
     spiegel: {
